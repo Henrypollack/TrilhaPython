8fcc2b5b923795f753a4d2b652cea11513e7a70a
Antonio <antonio@MacBook-de-Antonio.local>
2024-05-29 20:07:05 -0300

    finalizando o primeiro desafio :D

--- desafio2.py
+++ desafio2.py
@@ -6,15 +6,18 @@
     
 
 # Solicita ao usuário que insira o consumo médio mensal de dados:
-consumo = float(input("Digite a quantiade de consumo mensal: "))
+consumo = float(input("\033[32m Digite a quantiade de consumo médio mensal em GB que voce usa: \033[m "))
 def recomendar_plano(consumo):
-  if (consumo <= 10):
-    print(" Plano Essencial Fibra - 50Mbps")
-  elif ((consumo >= 10) or (consumo <= 20)):
-    print("Plano Prata Fibra - 100Mbps")
-  elif (consumo > 20):
-      print("Plano Premium Fibra - 300Mbps:")
-  return consumo   
+  if consumo <= 10:
+    print(f"""Com o consumo medio de {consumo}GB no mês indicamos voce usar o \033[35mPlano Essencial Fibra - 50Mbps.\033[m 
+Este plano ideal para você!.""")
+  elif consumo >= 10 and consumo <= 20:
+    print(f"""Com o consumo medio de {consumo}GB no mês indicamos voce usar o \033[30:47mPlano Prata Fibra - 100Mbps\033[m
+Este plano ideal para você!.""")
+  elif consumo >=20:
+      print(f"""Com o consumo medio de {consumo}GB no mês indicamos voce usar o \033[33mPlano Premium Fibra - 300Mbps.\033[m
+Este plano ideal para você!.""")
+     
 
 # Chama a função recomendar_plano com o consumo inserido e imprime o plano recomendado:
 print(recomendar_plano(consumo))
